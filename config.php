4ac511cacbc2c503bad834ab3f65e8e911acf253
Oleg9791 <oleg9791@tut.by>
2021-08-03 23:33:39 +0300

    added comments

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return [
     "servername" => "localhost",
     "username" => "root",
-    "password" => "",
+    "password" => "root",
     "dbname" => "w1020",
     "page_size" => 5,
     "salt"=>"kjsksdjt5859fdjkdfdfj"
